7999411ed53a2d29b85b2e0a77cf4c5868adbff6
姜辉 <hui.jiang@grandhonor.net>
2019-08-28 20:11:18 +0800

    nihao

--- src/main/java/com/example/concurrency/ConcurrencyApplication.java
+++ src/main/java/com/example/concurrency/ConcurrencyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConcurrencyApplication {
-
+	//22222
 	public static void main(String[] args) {
 		SpringApplication.run(ConcurrencyApplication.class, args);
 	}
