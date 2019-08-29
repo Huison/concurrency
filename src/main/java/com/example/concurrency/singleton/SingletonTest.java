0653ce12d64deb39705d575fe4b9cb8a167421ad
姜辉 <hui.jiang@grandhonor.net>
2019-08-29 17:56:28 +0800

    使用枚举实现单例模式最安全

---
+++ src/main/java/com/example/concurrency/singleton/SingletonTest.java
@@ -0,0 +1,54 @@
+package com.example.concurrency.singleton;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+//使用枚举实现单例模式最安全
+public class SingletonTest {
+
+    private SingletonTest(){}
+
+    private static SingletonTest singletonTest;
+
+    public static SingletonTest getInstance(){
+
+        return Singleton.INSTANCE.getInstance();
+    }
+
+    private  enum  Singleton{
+        INSTANCE;
+
+        private SingletonTest singletonTest;
+
+        //JVM保证这个方法只被调用一次
+        Singleton(){
+            singletonTest = new SingletonTest();
+        }
+
+        public SingletonTest getInstance(){
+            return singletonTest;
+        }
+    }
+
+}
+
+
+
+class Woshinibaba{
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.execute(()->{
+            SingletonTest singletonTest = SingletonTest.getInstance();
+            System.out.println(singletonTest.hashCode());
+        });
+
+        executorService.execute(()->{
+            SingletonTest singletonTest1 = SingletonTest.getInstance();
+            System.out.println(singletonTest1.hashCode());
+        });
+        executorService.shutdown();
+
+    }
+
+}
